81c212a37a47b624f344667a7e9732b1b4738fe7
dev-fildes <devafildes@gmail.com>
2020-04-25 14:09:03 -0400

    - removed semantics

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/profile/:id', to: 'homes#index'
   get '/posts/:id', to: 'homes#index'
 
+  resources :users, only: [:index]
+
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:index, :show, :create, :update, :destroy] do
